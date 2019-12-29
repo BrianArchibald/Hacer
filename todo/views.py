d5103ae768d58a6148cb38f1b2706713b05a4cc8
Brian Archibald <brian.archibald@imtapps.com>
2019-12-29 10:38:48 -0600

    First commit

---
+++ todo/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .models import TodoItem
+
+
+def todoView(request):
+    all_todo_items = TodoItem.objects.all()
+    return render(request, 'todo.html', {'all_items': all_todo_items})
+
+
+def addTodo(request):
+    # create new todo
+    # in post request find attribute content
+    new_item = TodoItem(content=request.POST['content'])
+    # save
+    new_item.save()
+    # redirect to page
+    return HttpResponseRedirect('/todo/')
+
+
+def deleteTodo(request, todo_id):
+    item_to_delete = TodoItem.objects.get(id=todo_id)
+    item_to_delete.delete()
+    return HttpResponseRedirect('/todo/')
